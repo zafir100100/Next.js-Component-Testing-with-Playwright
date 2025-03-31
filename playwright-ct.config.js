f9fdd26c683067997b1706957e31ee66efa45f6e
zafir100100 <55599023+zafir100100@users.noreply.github.com>
2025-03-31 17:33:13 +0600

    monocart-report added

--- playwright-ct.config.js
+++ playwright-ct.config.js
@@ -19,7 +19,15 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  // playwright.config.js
+  reporter: [
+      ['list'],
+      ['monocart-reporter', {  
+          name: "My Test Report",
+          outputFile: './monocart-report/index.html'
+      }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
